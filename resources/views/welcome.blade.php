90e4c6ae0a6dcee99a787fbc7780546e4bbd88f5
shenchaosong <shenchaosong@yidaoit.cn>
2021-01-02 22:56:09 +0800

    feature(question): 处理分享功能

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -293,10 +293,67 @@
     </div>
 </div>
 
-<script type="text/javascript" src="http://res.wx.qq.com/open/js/jweixin-1.2.0.js"></script>
+<script type="text/javascript" src="http://res.wx.qq.com/open/js/jweixin-1.6.0.js"></script>
 <script type="text/javascript" src="../../frontend/js/index.js"></script>
 <script>
-    $.ajax({
+
+
+    wx.config({
+        debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+        appId: "{{ $appId }}", // 必填，公众号的唯一标识
+        timestamp: {{$timestamp}}, // 必填，生成签名的时间戳
+        nonceStr: "{{$nonceStr}}", // 必填，生成签名的随机串
+        signature: "{{$signature}}",// 必填，签名
+        jsApiList: ['updateAppMessageShareData','updateTimelineShareData','checkJsApi',
+            'onMenuShareTimeline',
+            'onMenuShareAppMessage',
+            'startRecord',
+            'stopRecord',
+            'onVoiceRecordEnd',
+            'playVoice',
+            'onVoicePlayEnd',
+            'pauseVoice',
+            'stopVoice',
+            'uploadVoice',
+            'downloadVoice',
+            'onMenuShareTimeline',
+            'onMenuShareAppMessage',
+            'onMenuShareQQ',
+            'onMenuShareWeibo',
+            'hideMenuItems',
+            'showMenuItems'] // 必填，需要使用的JS接口列表
+    });
+    wx.ready(function () {   //需在用户可能点击分享按钮前就先调用
+        wx.updateAppMessageShareData({
+            title: '绿城•莫干山郡安里', // 分享标题
+            desc: '郡安里生活调研问卷', // 分享描述
+            link: "{{$url}}", // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+            imgUrl: 'http://fast.pinecc.cn/frontend/images/img.jpeg', // 分享图标
+         //   imgUrl: 'http://fast.pinecc.cn/frontend/images/imgUrl.png', // 分享图标
+            success: function () {
+                // 设置成功
+
+            },
+            error: function(data) {
+
+            }
+        });
+        wx.updateTimelineShareData({
+            title: '绿城•莫干山郡安里', // 分享标题
+            link: "{{$url}}", // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+            imgUrl: 'http://fast.pinecc.cn/frontend/images/img.jpeg', // 分享图标
+         //   imgUrl: 'http://fast.pinecc.cn/frontend/images/imgUrl.png', // 分享图标
+            success: function () {
+                // 设置成功
+
+            },
+            error: function(data) {
+
+            }
+        })
+    });
+
+   /* $.ajax({
         type: "GET",
         url: "",
         data: {
@@ -305,7 +362,7 @@
         dataType: "json",
         success: function(data) {
             wx.config({
-                debug: false,
+                debug: true,
                 appId: data.appId,
                 timestamp: data.timestamp,
                 nonceStr: data.nonceStr,
@@ -359,6 +416,7 @@
         error: function(data) {
         }
     })
+    console.log(123);*/
 
 </script>
 </body>
